159075126fc3fa242f6c7478c1f46070146302b7
Guilherme Nascimento da Silva <50491680+GuilhermeN391@users.noreply.github.com>
2026-04-09 21:41:50 -0300

    Rename resulSis.h to Execução/resulSis.h

--- Execução/resulSis.h
+++ Execução/resulSis.h
@@ -1,8 +1,8 @@
 struct matrizes gauss(struct matrizes a, struct matrizes b, struct matrizes c);
-/// Função que retorna o senh da variavel a inserida
+/// FunÃ§Ã£o que retorna o senh da variavel a inserida
 float senh(float a);
-/// Função para dar o valor de pi,uma vez que
-/// no c não tem esse número na sua biblioteca
+/// FunÃ§Ã£o para dar o valor de pi,uma vez que
+/// no c nÃ£o tem esse nÃºmero na sua biblioteca
 float pi();
 float ana(int n, float coorX, float coorY);
 struct matrizes multiana(int an);
